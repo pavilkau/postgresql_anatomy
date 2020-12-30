d9bb6709a5d162db3b450f4b3679b7832c1ae76b
Paulius Vilkauskas <pauliusvilkauskas@gmail.com>
2020-12-30 21:45:02 +0200

    Add dataset analyzer and suppression (#4)

---
+++ analyze.py
@@ -0,0 +1,43 @@
+CREATE or replace FUNCTION analyze_dataset_for_l(
+    table_name text,
+    sa_column_name text
+)
+RETURNS void AS $$
+plpy.info('\n\n\n')
+plpy.execute('select _meta()')
+
+# Fetch dataset size
+dataset_size = GD['fetch_table_size'](table_name)
+plpy.info("Dataset size: {}".format(dataset_size))
+
+# Fetch the distribution of sa attributes (distinct sa + count)
+sa_distribution = GD['fetch_sa_distribution'](table_name, sa_column_name)
+
+# Print eligible l options
+max_l_without_loss = dataset_size // max(sa_distribution.values())
+plpy.info("Max L without data loss = {}".format(max_l_without_loss))
+
+for l in range(1, max_l_without_loss + 1):
+    number_of_groups = dataset_size // l
+    plpy.info("L = {}".format(l) + " -> No. of groups = {}".format(number_of_groups))
+
+
+# Calculate data loss for non eligible l options
+number_of_distinct_sa = len(sa_distribution)
+
+for l in range(max_l_without_loss + 1, number_of_distinct_sa + 1):
+    data_loss, _ = GD['calculate_data_loss'](l, dataset_size, sa_distribution)
+
+    updated_dataset_size = dataset_size - data_loss
+    number_of_groups = updated_dataset_size // l
+
+    plpy.info("L = {}".format(l) +
+        " -> No. of suppressed records = {}".format(data_loss) +
+        "; No. of groups = {}".format(number_of_groups))
+
+$$
+LANGUAGE plpythonu;
+
+
+select analyze_dataset_for_l('bank_churners', 'income_category');
+
--- mainf.py
+++ mainf.py
@@ -10,16 +10,26 @@ CREATE or replace FUNCTION mainfunc(
     sa_table_name text default 'sa_table'
 )
 RETURNS void AS $$
-plpy.info('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+# Usage:
+# select mainfunc('main_table', 'disease', '{"*"}', 2);
+# select mainfunc('bank_churners', 'income_category', '{"*"}', 5);
+
+plpy.info('\n\n\n\n')
+
 
 # *****************************************************************************************
-# Init functions
+# L parameter eligibility check
 # *****************************************************************************************
-plpy.execute('select _meta()')
+
+sa_distribution = GD['fetch_sa_distribution'](table_name, sa_name)
+max_l = len(sa_distribution)
+if l_level > max_l:
+    error_message = "Can't anatomize with this l_level. Max possible l_level = {}".format(max_l)
+    plpy.error(error_message)
 
 
 # *****************************************************************************************
-# Database preparation (fetch  column types, create qi & sa tables)
+# Database preparation (fetch column types, create qi & sa tables)
 # *****************************************************************************************
 
 qi_column_metadata, sa_metadata = GD['fetch_column_metadata'](schema, table_name, sa_name, qi_columns)
@@ -33,11 +43,32 @@ if create_sa_table == True:
 
 
 # *****************************************************************************************
+# Data supression. If distribution eligibility requirement is not met - suppress overpopulated SA attributes
+# *****************************************************************************************
+
+dataset_size = GD['fetch_table_size'](table_name)
+max_l_without_loss = dataset_size // max(sa_distribution.values())
+
+
+if max_l_without_loss < l_level:
+    temp_table_name = "temp_{}".format(table_name)
+    plpy.execute("create table {} as (select * from {})".format(temp_table_name, table_name))
+
+    _, sa_delta_hash = GD['calculate_data_loss'](l_level, dataset_size, sa_distribution)
+    GD['suppress_dataset'](temp_table_name, sa_delta_hash, sa_name)
+
+    data = plpy.execute("select * from {}".format(temp_table_name))
+    plpy.execute("drop table {}".format(temp_table_name))
+else:
+    data = plpy.execute("select * from {}".format(table_name))
+
+
+# *****************************************************************************************
 # Anatomizaton algorithm
 # *****************************************************************************************
 
 # Hash the tuples in T (rows) by their As (sensitive attr) values (each bucket per As value)
-data = plpy.execute("select * from {}".format(table_name))
+# data = plpy.execute("select * from {}".format(table_name))
 buckets = GD["hash_tuples_into_buckets"](data, sa_name)
 
 # Create QI groups
@@ -65,6 +96,3 @@ for row in list_of_sa:
 
 $$
 LANGUAGE plpythonu;
-
-# select mainfunc('main_table', 'disease', '{"*"}', 2);
-# select mainfunc('bank_churners', 'income_category', '{"*"}', 3);
