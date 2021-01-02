c3dc455b7cdafac9453162c4fa77b71b1de31aa3
pavilkau <pauliusvilkauskas@gmail.com>
2021-01-02 18:53:20 +0200

    prog

--- parse_dataset.py
+++ parse_dataset.py
@@ -6,7 +6,3 @@ df = pd.read_csv(file_name, index_col=False)
 columns = ["Customer_Age","Gender","Dependent_count","Education_Level","Marital_Status","Income_Category"]
 
 df.to_csv('output.csv', columns = columns, index=False)
-
-
-# create table bank_churners(id serial, age integer, gender text, dependent_count integer, education text, marital_status text, income_category text);
-# copy bank_churners(age, gender, dependent_count, education, marital_status, income_category) from '/home/chridcrow/anatomy_gdpr_ba/output.csv' delimiter ',' csv header;
